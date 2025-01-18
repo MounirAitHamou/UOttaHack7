11568bc43706ddb2d27307a4ba683160ebac234e
Alvin Tran <101605216+alvin69420@users.noreply.github.com>
2025-01-18 13:26:26 -0500

    Updating Gamestate.py to add bestMove

--- Shared_Util/Gamestate.py
+++ Shared_Util/Gamestate.py
@@ -7,8 +7,10 @@ class Gamestate:
                  playfield: list[list[Pixel]] = [[Pixel() for _ in range(20)] for _ in range(10)],
                  holdPiece: Pieces = None,
                  nextPieces: list[Pieces] = [Piece.generateRandomPiece() for _ in range(3)],
-                 score: int = 0):
+                 score: int = 0,
+                 bestMove: Piece = None):
         self.playfield = playfield
         self.holdPiece = holdPiece
         self.nextPieces = nextPieces
         self.score = score
+        self.bestMove = bestMove
