a86ea9adf6e911fd3ae1288dee808fc1069f97eb
LaGa-Mi <lauriergascon@gmail.com>
2025-01-18 19:03:09 -0500

    Removed sharedUtils, will be replaced soon

--- Shared_Util/Pixel.py
+++
@@ -1,8 +0,0 @@
-from Shared_Util.Colors import Colors
-
-class Pixel:
-    def __init__(self,
-                 filled: bool = False,
-                 color: Colors = Colors.EMPTY):
-        self.filled = filled
-        self.color = color
