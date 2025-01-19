39f858b3e5f9267ebdecbd9540a0fa5d9a623659
MounirAitHamou <mounir.aithamou@gmail.com>
2025-01-18 23:47:00 -0500

    Update AI.py

--- AI/AI.py
+++ AI/AI.py
@@ -1,129 +1,93 @@
 from Game.components.block import Block
 from Game.components.grid import Grid
+from Game.Trainer import Trainer
+from GameComponentUtils import cloneBlock, cloneGrid, lockBlock
+from MoveParameters import MoveParameters
+import copy
 
 class AI:
     def __init__(self, candidate):
-        self.heightWeight = candidate.heightWeight
-        self.completeLinesWeight = candidate.completeLinesWeight
-        self.holesWeight = candidate.holesWeight
-        self.bumpinessWeight = candidate.bumpinessWeight
+        self.heightWeight = candidate["heightWeight"]
+        self.completeLinesWeight = candidate["completeLinesWeight"]
+        self.holesWeight = candidate["holesWeight"]
+        self.bumpinessWeight = candidate["bumpinessWeight"]
 
-    def getMove(self, grid: Grid, block: Block):
-        pass
-
-
-
-
-
-
+    def best(self, trainer:Trainer, playingPieces):
+        dupeTrainer = copy.deepcopy(trainer)
+        playingPieceIndex = 0
+        best = None
+        bestScore = None
+        playingPiece = playingPieces[playingPieceIndex]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for rotation in range(4):
+            piece = cloneBlock(playingPiece)
+            for _ in range(rotation):
+                piece.rotate()
+            dupeTrainer.attemptToMoveLeft(piece)
+            
+            
+            while(dupeTrainer.checkValid(piece)):
+                pieceCopy = cloneBlock(piece)
+                dupetrainer.moveDown(pieceCopy)
+                gridClone = cloneGrid(trainer.game.grid)
+                lockBlock(pieceCopy, gridClone)
+                score = None
+                moveParameters = getMoveParameters(gridClone)
+                if playingPieceIndex == (len(playingPieces) - 1):
+                    score = -self.heightWeight * moveParameters.AggregateHeight + self.completeLinesWeight * moveParameters.CompleteLines - self.holesWeight * moveParameters.Holes - self.bumpinessWeight * moveParameters.Bumpiness
+                else:
+                    score = self.best(dupeTrainer, playingPieces[playingPieceIndex + 1])
+                if score>bestScore or bestScore == None:
+                    bestScore = score
+                    best = cloneBlock(pieceCopy)
+                dupeTrainer.moveRight(piece)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-def getMoveParameters(grid: Grid, block: Block):
-    moveParameters = []
-
-    for rotationCount in range(4):
-        for x in range(10):
-            # If move is impossible, don't attempt it
-            if not canMove(gameState, block):
+def getMoveParameters(grid: Grid):
+    aggregateHeight = 0
+    for x in range(10):
+        for y in range(20):
+            if grid[y][x].filled:
+                aggregateHeight += 20 - y
                 break
 
-            # Copy the playfield to avoid modifying the original
-            gameStateCopy: GameState = GameState()
-            gameStateCopy.playfield = [[gameState.playfield[y][x] for x in range(10)] for y in range(20)]
-
-            # Drop block as if it were played
-            dropPiece(gameStateCopy, block)
-
-            # Calculate the Aggregate Height
-            aggregateHeight = 0
-            for x in range(10):
-                for y in range(20):
-                    if gameStateCopy.playfield[y][x].filled:
-                        aggregateHeight += 20 - y
+    totalCompleteLines: int = 0
+    for y in range(20):
+        lineComplete: bool = True
+        for x in range(10):
+            if not grid[y][x].filled:
+                lineComplete = False
+                break
+        if lineComplete:
+            totalCompleteLines += 1
+    
+    totalHoles: int = 0
+    for y in range(1, 20):
+        for x in range(10):
+            if not grid[y][x].filled:
+                isHole: bool = False
+                for y2 in range(y - 1, 0, -1):
+                    if grid[y2][x].filled:
+                        isHole = True
                         break
-            print("Aggregate height: " + str(aggregateHeight))
+                if (isHole):
+                    totalHoles += 1
 
-            # Calculate the Complete Lines
-            totalCompleteLines: int = 0
-            for y in range(20):
-                lineComplete: bool = True
-                for x in range(10):
-                    if not gameStateCopy.playfield[y][x].filled:
-                        lineComplete = False
-                        break
-                if lineComplete:
-                    totalCompleteLines += 1
-            print("Complete lines: " + str(totalCompleteLines))
-
-            # Count number of holes
-            totalHoles: int = 0
-            for y in range(1, 20):
-                for x in range(10):
-                    if not gameStateCopy.playfield[y][x].filled:
-                        isHole: bool = False
-                        for y2 in range(y - 1, 0, -1):
-                            if gameStateCopy.playfield[y2][x].filled:
-                                isHole = True
-                                break
-                        if (isHole):
-                            totalHoles += 1
-            print("Total holes: " + str(totalHoles))
-
-            # Calculate the Bumpiness
-            bumpiness: int = 0
-            previousColumnHeight: int
-            for y in range(20):
-                if gameStateCopy.playfield[y][0].filled:
-                    previousColumnHeight = 20 - y
-                    break
-            for x in range(1, 10):
-                for y in range(20):
-                    if gameStateCopy.playfield[y][x].filled or y == 19:
-                        bumpiness += abs(previousColumnHeight - (20 - y))
-                        previousColumnHeight = 20 - y
-                        break
-            
-
-
-            return MoveParameters(aggregateHeight, totalCompleteLines, totalHoles, bumpiness)
+    bumpiness: int = 0
+    previousColumnHeight: int
+    for y in range(20):
+        if grid[y][0].filled:
+            previousColumnHeight = 20 - y
+            break
+    for x in range(1, 10):
+        for y in range(20):
+            if grid[y][x].filled or y == 19:
+                bumpiness += abs(previousColumnHeight - (20 - y))
+                previousColumnHeight = 20 - y
+                break
+    return MoveParameters(aggregateHeight, totalCompleteLines, totalHoles, bumpiness)
 
 
