f9c7ae6178123c1f19bd03683fab5cd9ba15c714
MounirAitHamou <mounir.aithamou@gmail.com>
2025-01-18 22:08:11 -0500

    Update Tuner.py

--- AI/Tuner.py
+++ AI/Tuner.py
@@ -1,7 +1,7 @@
 import random as random
 import math as math
 from AI.AI import AI
-from Controller.Controller import Controller
+from Game.controller import Controller
 def randomInteger(min, max):
     return math.floor(random.random() * (max - min ) + min)
 
